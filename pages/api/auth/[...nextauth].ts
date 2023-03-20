33b765b8e5a2a859759de479027b446c3ab488c0
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-03-20 20:06:56 +0530

    added register functionality with next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt";
+import prismadb from "../../../lib/prismadb";
+
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        //check if all fields are provided.
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
+        // look up the user from the credentials supplied
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        //If no user found
+        if (!user || !user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
+        //user found and correct password
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        //If incorrect password.
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+//   adapter: PrismaAdapter(prismadb),
+  session: { strategy: "jwt" },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useCallback } from "react";
 import Input from "../components/Input";
 
@@ -13,6 +14,20 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+
+   
+    } catch (error) {
+        console.log(error);
+    }
+  }, [email, name, password]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -50,7 +65,7 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === "login" ? "Login" : "Sign up"}
             </button>
             <p className="text-neutral-500 mt-12">
