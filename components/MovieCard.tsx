f04e0a91a7a15cfa4897cf7e34f2b1edc73d1425
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-03-21 22:23:47 +0530

    Added Modal for Info

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,9 +1,10 @@
 import { fill } from "lodash";
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
-import { GrFavorite } from "react-icons/gr";
+import { BiChevronDown } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
 import { useRouter } from "next/router";
+import useInfoModalStore from "../hooks/useInfoModel";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -11,6 +12,7 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+  const { openModal } = useInfoModalStore();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -88,6 +90,12 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30} className="text-black w-4 lg:w-6" />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+            >
+              <BiChevronDown className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
